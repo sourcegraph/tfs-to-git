985bc16cbe373adf136de9c7f99124f28a7c5e71
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2024-12-19 04:00:34 -0700

    Fix PID

--- tfs-to-git.sh
+++ tfs-to-git.sh
@@ -664,7 +664,8 @@ function check_or_set_lock_file() {
     # date +%s > "$lock_file_path"
 
     # Create the lock file, and write the current PID to it so we can check if the script is still running
-    current_pid="$($$)"
+    current_pid="$BASHPID"
+
     echo "$current_pid" > "$lock_file_path"
 
     debug "Created lock file at $lock_file_path, with PID $current_pid"
